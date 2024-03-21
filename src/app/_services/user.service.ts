61389dfb348b5a0fb97a524a9b83ffc9063f4e18
Lucas Motta <mottadefreitaslucasgabriel2004@gmail.com>
2024-03-21 19:57:35 -0300

    Create user service that will call the user resource.

---
+++ src/app/_services/user.service.ts
@@ -0,0 +1,30 @@
+import {Component, Injectable} from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { Observable } from "rxjs";
+
+const API_URL = 'http://localhost:8080/api/'
+
+@Injectable({
+    providedIn: 'root',
+})
+export class UserService {
+    constructor(private http: HttpClient) {
+    }
+
+    public getAll() : Observable<any> {
+        return this.http.get(API_URL, { responseType: 'text' })
+    }
+
+    public getByOid(oid: String) : Observable<any> {
+        return this.http.get(API_URL + oid, { responseType: 'text' });
+    }
+
+    public getByEmail(email: String) : Observable<any> {
+        return this.http.get(API_URL + email, { responseType: 'text' });
+    }
+
+    public getByUsername(username: String) : Observable<any> {
+        return this.http.get(API_URL + username, { responseType: 'text'} )
+    }
+
+}
